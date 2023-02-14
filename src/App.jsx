b22cae4328ade337d286251b4f8b8a23f8bc539b
Rodrigo Enrique Alfaro <rodrigoenriquealfaro@hotmail.com>
2023-02-14 19:34:32 -0300

    Aprendiendo sobre el recorrido de arreglos con MAP

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+const nombre = "Rodrigo"
+const color = "blue"
+const sesion = true
+const pais = null
+const amigos = ['Alejandro', 'Manuel', 'Cesar']
+
 const App = () => {
     return (
-        <div>
+        <>
+            {sesion
+                ?
+                <>
+                    <p>Has iniciado sesión</p>
+                    <h1 className="titulo" style={{ color: color }}>Hola {nombre}!</h1>
+                    <p>Que tengas buen día</p>
+                    {pais && <p>Tu eres de: {pais}</p>}
+                    <ul>
+                        {amigos.map((amigo, index) => (
+                            <li key={index}>{amigo}</li>
+                        ))}
+                    </ul>
+                </>
+                : <p>No has iniciado sesión</p>}
 
-        </div>
+        </>
     )
 }
 
