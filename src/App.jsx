a5ce5285913f96bad920ed8871e7658ad458024f
Rodrigo Enrique Alfaro <rodrigoenriquealfaro@hotmail.com>
2023-02-14 20:59:51 -0300

    Introducción a los componentes en React

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
 const nombre = "Rodrigo"
-const color = "blue"
+const color = "red"
 const sesion = true
 const pais = null
 const amigos = ['Alejandro', 'Manuel', 'Cesar']
 
+const Usuario = () => {
+    return (
+        <div>
+            <h1 className="titulo" style={{ color: color }}>Hola {nombre}!</h1>
+            <p>Tu lista de amigos es:</p>
+            {pais && <p>Tu eres de: {pais}</p>}
+            <ul>
+                {amigos.map((amigo, index) => (<li key={index}>{amigo}</li>))}
+            </ul>
+            <p>Que tengas un buen día.</p>
+        </div>
+    )
+}
+
 const App = () => {
     return (
         <>
-            {sesion
-                ?
-                <>
-                    <p>Has iniciado sesión</p>
-                    <h1 className="titulo" style={{ color: color }}>Hola {nombre}!</h1>
-                    <p>Que tengas buen día</p>
-                    {pais && <p>Tu eres de: {pais}</p>}
-                    <ul>
-                        {amigos.map((amigo, index) => (
-                            <li key={index}>{amigo}</li>
-                        ))}
-                    </ul>
-                </>
-                : <p>No has iniciado sesión</p>}
+            {sesion ? <Usuario /> : <p>No has iniciado sesión</p>}
 
         </>
     )
